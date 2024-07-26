acbaec5f12e73f6d7b6aeab7706f9964a0721ba6
JaatadiaMulesoft <35300185+JaatadiaMulesoft@users.noreply.github.com>
2024-07-26 13:33:06 -0300

    W-16238279: Update versions. (#49)

--- metrics/tests/requests.rs
+++ metrics/tests/requests.rs
@@ -4,15 +4,12 @@ mod common;
 
 use httpmock::MockServer;
 use pdk_test::port::Port;
-use pdk_test::services::flex::{Flex, FlexConfig};
+use pdk_test::services::flex::{ApiConfig, Flex, FlexConfig, PolicyConfig};
 use pdk_test::services::httpmock::{HttpMock, HttpMockConfig};
 use pdk_test::{pdk_test, TestComposite};
 use std::time::Duration;
 
 use common::*;
-
-// Directory with the configurations for the `hello` test.
-const HELLO_CONFIG_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/requests/hello");
 
 // Flex port for the internal test network
 const FLEX_PORT: Port = 8081;
@@ -21,23 +18,35 @@ const FLEX_PORT: Port = 8081;
 // with a MockServer backend
 #[pdk_test]
 async fn metrics() -> anyhow::Result<()> {
-    // Configure a Flex service
-    let flex_config = FlexConfig::builder()
-        .version("1.7.0")
-        .hostname("local-flex")
-        .ports([FLEX_PORT])
-        .config_mounts([
-            (POLICY_DIR, "policy"),
-            (COMMON_CONFIG_DIR, "common"),
-            (HELLO_CONFIG_DIR, "hello"),
-        ])
-        .build();
-
     // Configure an HttpMock service
     let httpmock_config = HttpMockConfig::builder()
         .port(80)
         .version("latest")
         .hostname("backend")
+        .build();
+
+    // Configure a Flex service
+    let policy_config = PolicyConfig::builder()
+        .name(POLICY_NAME)
+        .configuration(serde_json::json!({
+            "metricsSink": "http://backend/anything/echo/metrics",
+            "pushFrequency": 1
+        }))
+        .build();
+
+    let api_config = ApiConfig::builder()
+        .name("ingress-http")
+        .upstream(&httpmock_config)
+        .path("/anything/echo/")
+        .port(FLEX_PORT)
+        .policies([policy_config])
+        .build();
+
+    let flex_config = FlexConfig::builder()
+        .version("1.7.0")
+        .hostname("local-flex")
+        .with_api(api_config)
+        .config_mounts([(POLICY_DIR, "policy"), (COMMON_CONFIG_DIR, "common")])
         .build();
 
     // Compose the services
