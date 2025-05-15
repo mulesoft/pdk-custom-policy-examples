199d1932068ff86ebc84dc449c271ecbba05bad2
Martina Ágata <mpanetta@salesforce.com>
2025-05-15 14:05:22 -0300

    Update wasm32-wasi to wasm32-wasip1 and fix rust-version typos

--- stream-payload/tests/common.rs
+++ stream-payload/tests/common.rs
@@ -3,7 +3,7 @@
 // This module contains common Rust stuff shared between test files.
 
 // Directory where the policies implementations are stored.
-pub const POLICY_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/target/wasm32-wasi/release");
+pub const POLICY_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/target/wasm32-wasip1/release");
 
 // Directory with the common configurations for tests.
 pub const COMMON_CONFIG_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/common");
