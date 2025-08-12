b30db0a156a95d2c356437638fa8df452081bde0
Martina Ágata <39577698+martinaAgata@users.noreply.github.com>
2025-08-12 16:17:55 -0300

    W-18697356: Update flex version to 1.10.0 (#88)

--- simple-oauth-2-validation-grpc/tests/requests.rs
+++ simple-oauth-2-validation-grpc/tests/requests.rs
@@ -57,7 +57,7 @@ async fn accept_token() -> anyhow::Result<()> {
 
     // Configure Flex Gateway
     let flex_config = FlexConfig::builder()
-        .version("1.7.0")
+        .version("1.10.0")
         .hostname("local-flex")
         .with_api(api_config)
         .config_mounts([(POLICY_DIR, "policy"), (COMMON_CONFIG_DIR, "common")])
