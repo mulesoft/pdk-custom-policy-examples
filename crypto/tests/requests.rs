50454a9c25bd99ac9614ef0f0d484d3a327debcb
Martina Ágata <39577698+martinaAgata@users.noreply.github.com>
2025-08-13 12:03:32 -0300

    W-18697356: Update flex version to 1.10.0 (cherry-pick) (#88) (#91)

--- crypto/tests/requests.rs
+++ crypto/tests/requests.rs
@@ -90,7 +90,7 @@ async fn crypto() -> anyhow::Result<()> {
         .build();
 
     let flex_config = FlexConfig::builder()
-        .version("1.7.0")
+        .version("1.10.0")
         .hostname("local-flex")
         .with_api(api_config)
         .config_mounts([(POLICY_DIR, "policy"), (COMMON_CONFIG_DIR, "common")])
--- stream-payload/tests/requests.rs
+++ stream-payload/tests/requests.rs
@@ -42,7 +42,7 @@ async fn reject_forbidden() -> anyhow::Result<()> {
 
     // Configure a Flex service
     let flex_config = FlexConfig::builder()
-        .version("1.7.0")
+        .version("1.10.0")
         .hostname("local-flex")
         .with_api(api_config)
         .config_mounts([(POLICY_DIR, "policy"), (COMMON_CONFIG_DIR, "common")])
