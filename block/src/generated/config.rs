4c6f743cf4209ed4dd057f83f41f7ae17a5e19da
Martina Ágata <mpanetta@salesforce.com>
2025-11-26 12:27:14 -0300

    Format changes after make build

--- block/src/generated/config.rs
+++ block/src/generated/config.rs
@@ -10,13 +10,13 @@ pub struct Config {
 }
 #[pdk::hl::entrypoint_flex]
 fn init(abi: &dyn pdk::flex_abi::api::FlexAbi) -> Result<(), anyhow::Error> {
-    let config: Config = serde_json::from_slice(abi.get_configuration()).map_err(|err| {
-        anyhow::anyhow!(
-            "Failed to parse configuration '{}'. Cause: {}",
-            String::from_utf8_lossy(abi.get_configuration()),
-            err
-        )
-    })?;
+    let config: Config = serde_json::from_slice(abi.get_configuration())
+        .map_err(|err| {
+            anyhow::anyhow!(
+                "Failed to parse configuration '{}'. Cause: {}",
+                String::from_utf8_lossy(abi.get_configuration()), err
+            )
+        })?;
     abi.service_create(config.source)?;
     abi.setup()?;
     Ok(())
@@ -25,7 +25,9 @@ fn de_ip_0<'de, D>(deserializer: D) -> Result<pdk::script::Script, D::Error>
 where
     D: serde::de::Deserializer<'de>,
 {
-    let exp: pdk::script::Expression = serde::de::Deserialize::deserialize(deserializer)?;
+    let exp: pdk::script::Expression = serde::de::Deserialize::deserialize(
+        deserializer,
+    )?;
     pdk::script::ScriptingEngine::script(&exp)
         .input(pdk::script::Input::Attributes)
         .input(pdk::script::Input::Payload(pdk::script::Format::PlainText))
