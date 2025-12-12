03ef318713de7865af7ed6d280cf596cdf7c35c8
Martina Ágata <mpanetta@salesforce.com>
2025-12-12 13:42:55 -0300

    fmt

--- ip-allowlist/tests/requests.rs
+++ ip-allowlist/tests/requests.rs
@@ -3,10 +3,10 @@
 mod common;
 
 use httpmock::MockServer;
-use pdk_test::{pdk_test, TestComposite};
 use pdk_test::port::Port;
-use pdk_test::services::flex::{ApiConfig, FlexConfig, Flex, PolicyConfig};
-use pdk_test::services::httpmock::{HttpMockConfig, HttpMock};
+use pdk_test::services::flex::{ApiConfig, Flex, FlexConfig, PolicyConfig};
+use pdk_test::services::httpmock::{HttpMock, HttpMockConfig};
+use pdk_test::{pdk_test, TestComposite};
 
 use common::*;
 
@@ -17,7 +17,6 @@ const FLEX_PORT: Port = 8081;
 // with a MockServer backend
 #[pdk_test]
 async fn hello() -> anyhow::Result<()> {
-
     // Configure an HttpMock service
     let httpmock_config = HttpMockConfig::builder()
         .port(80)
@@ -43,10 +42,7 @@ async fn hello() -> anyhow::Result<()> {
         .version("1.10.0")
         .hostname("local-flex")
         .with_api(api_config)
-        .config_mounts([
-            (POLICY_DIR, "policy"),
-            (COMMON_CONFIG_DIR, "common"),
-        ])
+        .config_mounts([(POLICY_DIR, "policy"), (COMMON_CONFIG_DIR, "common")])
         .build();
 
     // Compose the services
@@ -69,10 +65,12 @@ async fn hello() -> anyhow::Result<()> {
     let mock_server = MockServer::connect_async(httpmock.socket()).await;
 
     // Mock a /hello request
-    mock_server.mock_async(|when, then| {
-        when.path_contains("/hello");
-        then.status(202).body("World!");
-    }).await;
+    mock_server
+        .mock_async(|when, then| {
+            when.path_contains("/hello");
+            then.status(202).body("World!");
+        })
+        .await;
 
     // Perform an actual request
     let response = reqwest::get(format!("{flex_url}/hello")).await?;
